c88624cd4fcbaeb6c3b6e9da417cd87ab77ba946
Daniel Vasquez <dvasquez447@yahoo.com>
2017-08-20 19:01:27 -0500

    Challenge Complete

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView mStoryTextView;
+    Button mTopAnswer;
+    Button mBottomAnswer;
+    int mStoryIndex = 1;
+
 
 
     @Override
@@ -15,16 +23,62 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        mStoryTextView = (TextView)findViewById(R.id.storyTextView);
+        mTopAnswer = (Button)findViewById(R.id.buttonTop);
+        mBottomAnswer = (Button)findViewById(R.id.buttonBottom);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mTopAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex == 1){
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mTopAnswer.setText(R.string.T3_Ans1);
+                    mBottomAnswer.setText(R.string.T3_Ans2);
+                    mStoryIndex = 3;
+                }else if(mStoryIndex == 2){
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mTopAnswer.setText(R.string.T3_Ans1);
+                    mBottomAnswer.setText(R.string.T3_Ans2);
+                    mStoryIndex = 3;
+                }else if(mStoryIndex == 3){
+                    mStoryTextView.setText(R.string.T6_End);
+                    mTopAnswer.setVisibility(View.GONE);
+                    mBottomAnswer.setVisibility(View.GONE);
+                    mStoryIndex=6;
+                }
+            }
+        });
+
 
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
+        mBottomAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex == 1){
+                    mStoryTextView.setText(R.string.T2_Story);
+                    mTopAnswer.setText(R.string.T2_Ans1);
+                    mBottomAnswer.setText(R.string.T2_Ans2);
+                    mStoryIndex = 2;
+                }else if(mStoryIndex == 2){
+                    mStoryTextView.setText(R.string.T4_End);
+                    mTopAnswer.setVisibility(View.GONE);
+                    mBottomAnswer.setVisibility(View.GONE);
+                    mStoryIndex = 4;
+                }else if(mStoryIndex == 3){
+                    mStoryTextView.setText(R.string.T5_End);
+                    mTopAnswer.setVisibility(View.GONE);
+                    mBottomAnswer.setVisibility(View.GONE);
+                    mStoryIndex=5;
+                }
+            }
+        });
 
     }
+
+
 }
---
+++ app/src/main/java/com/londonappbrewery/destini/Story.java
@@ -0,0 +1,21 @@
+package com.londonappbrewery.destini;
+
+/**
+ * Created by usuario on 8/20/2017.
+ */
+
+public class Story {
+    private int mStoryID;
+
+    public Story(int storyID) {
+        mStoryID = storyID;
+    }
+
+    public int getStoryID() {
+        return mStoryID;
+    }
+
+    public void setStoryID(int storyID) {
+        mStoryID = storyID;
+    }
+}
